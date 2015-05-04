6a67de24d089a5baa49b5693951947b5b2bd89df
mambax7 <mambax7@gmail.com>
2015-05-04 05:45:28 -0400

    Original files

---
+++ gray2/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="gray2"
+Depends=Legacy_RenderSystem
+Url="http://2bcool.net/"
+Version="1.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=Masanao Takahashi @ BCOOL Inc.
+ScreenShot="screenshot.png"
+Description="1,2,3 columns, flex or fixed width style theme"
+W3C=
+
+License="Attribution-NonCommercial-ShareAlike 2.1 Japan"
+
+*/
+?>
